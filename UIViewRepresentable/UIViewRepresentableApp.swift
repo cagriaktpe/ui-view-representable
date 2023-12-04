ac14bb72a66aa73afff9d2ca5d1ef04b8f9e1421
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-12-04 12:16:18 +0300

    Initial Commit

---
+++ UIViewRepresentable/UIViewRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewRepresentableApp.swift
+//  UIViewRepresentable
+//
+//  Created by Samet Çağrı Aktepe on 4.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct UIViewRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
