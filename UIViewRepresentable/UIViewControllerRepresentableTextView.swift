6af0f5ff1ac38e2fb498989079e85a0baac4e7fc
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-12-05 11:04:43 +0300

    UIImagePickerControllerRepresentable

--- UIViewRepresentable/UIViewControllerRepresentableTextView.swift
+++ UIViewRepresentable/UIViewControllerRepresentableTextView.swift
@@ -10,16 +10,23 @@ import SwiftUI
 struct UIViewControllerRepresentableTextView: View {
     
     @State private var showScreen: Bool = false
+    @State private var image: UIImage? = nil
     
     var body: some View {
         VStack {
             Text("Hello, world!")
             
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }
+            
             Button("Click me") {
                 showScreen.toggle()
             }
             .sheet(isPresented: $showScreen, content: {
-                BasicUIViewControllerRepresentable(labelText: "Hello, world!")
+                UIImagePickerControllerRepresentable(image: $image, showScreen: $showScreen)
             })
         }
     }
@@ -41,6 +48,46 @@ struct BasicUIViewControllerRepresentable: UIViewControllerRepresentable {
     }
 }
 
+struct UIImagePickerControllerRepresentable: UIViewControllerRepresentable {
+    
+    @Binding var image: UIImage?
+    @Binding var showScreen: Bool
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let vc = UIImagePickerController()
+        vc.allowsEditing = false
+        vc.delegate = context.coordinator
+        return vc
+    }
+    
+    // data from SwiftUI to UIKit
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    // data from UIKit to SwiftUI
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(image: $image, showScreen: $showScreen)
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        
+        @Binding var image: UIImage?
+        @Binding var showScreen: Bool
+        
+        init(image: Binding<UIImage?>, showScreen: Binding<Bool>) {
+            self._image = image
+            self._showScreen = showScreen
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let newImage = (info[.originalImage] as? UIImage) else { return }
+            image = newImage
+            showScreen.toggle()
+        }
+    }
+}
+
 class MyFirstViewController: UIViewController {
     
     var labelText: String = "Starting value"
@@ -58,6 +105,8 @@ class MyFirstViewController: UIViewController {
         label.frame = view.frame
     }
 }
+
+
 
 
 #Preview {
